87541ce00feab877c06946cfe04b48160fbcb1e5
Jayden Kim <hkim@lcs.on.ca>
2024-12-17 09:06:23 -0500

    Initial Commit

---
+++ NBA/NBAApp.swift
@@ -0,0 +1,17 @@
+//
+//  NBAApp.swift
+//  NBA
+//
+//  Created by kim heejun on 12/17/24.
+//
+
+import SwiftUI
+
+@main
+struct NBAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
